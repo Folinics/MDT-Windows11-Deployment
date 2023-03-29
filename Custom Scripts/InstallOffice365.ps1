c880333c6f59bea14c0f8d1e03d5508cd3322495
Jordan <jordan@buenoconnection.com>
2023-03-28 22:31:55 -0500

    Finished Microsoft 365 script

--- Custom Scripts/InstallOffice365.ps1
+++ Custom Scripts/InstallOffice365.ps1
@@ -1,13 +1,18 @@
-# Make temp directory 
+# Make temp directory
 New-Item -Path 'C:\Microsoft365Installation' -ItemType directory
 
 # Copy items
 Copy-Item "\\wds002-bueno\Deploy$\Custom Scripts\Microsoft365Installation\*" "C:\Microsoft365Installation" -Recurse
 
 # Change location
-Set-Location C:\Microsoft365Installation
+Set-Location C:\Microsoft365Installation\
 
 # Start Office download
-setup.exe /download configuration-Office365-x64.xml
+./setup.exe /download M365-x64-Custom.xml
 
+# Install Office
+./setup.exe /configure M365-x64-Custom.xml
 
+# Clean up
+cd C:\
+Remove-Item "C:\Microsoft365Installation\" -Recurse -Force
