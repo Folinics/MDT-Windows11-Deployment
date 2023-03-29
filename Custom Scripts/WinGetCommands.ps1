91fe354b6966cd8accf341cd8dddf2e951b06c06
Jordan <jordan@buenoconnection.com>
2023-03-28 21:56:43 -0500

    Added automated install of several programs

---
+++ Custom Scripts/WinGetCommands.ps1
@@ -0,0 +1,4 @@
+winget install --accept-source-agreements --accept-package-agreements Google.Chrome
+winget install --accept-source-agreements --accept-package-agreements Valve.Steam
+winget install --accept-source-agreements --accept-package-agreements LGHub
+winget install --accept-source-agreements --accept-package-agreements WinDirStat
